681d988a19a2fc5eb6d5ce8a7917a9e4412188c5
idomshi <56523771+idomshi@users.noreply.github.com>
2023-04-15 10:49:15 +0900

    子コンポーネントにSVGを追加してみます
    ……SVGが表示されない？

--- src/JitoEyes.ts
+++ src/JitoEyes.ts
@@ -15,7 +15,7 @@ const main: Main = ({ root }) => {
   }
 
   const patched = () => {
-    state.el = root.getElementById('id')
+    state.el = root.getElementById('jitoeyes')
   }
 
   return [
@@ -30,6 +30,9 @@ const main: Main = ({ root }) => {
 export const JitoEyes = compact(`
 <window onpointermove="onMove(event)" />
 <root onpatch="patched(event)" @if="!el" />
-<div id="id">
-  <strong>hello</strong>
+<div id="jitoeyes" style="width: 400px; height: 400px;">
+  <svg width="300" height="300" viewBox="0 0 300 100" xmlns="http://www.w3.org/2000/svg" stroke="red" fill="grey">
+    <circle cx="50" cy="50" r="40" />
+    <circle cx="150" cy="50" r="4" />
+  </svg>
 </div>`, main)
